f61d1b2dfd3d64afa2c5984cc904a35040ada261
Chester Agsamosam <chesteragsamosam@gmail.com>
2023-04-15 16:40:02 +0800

    clean

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -13,30 +13,11 @@ const count = ref(0);
 
 	<div class="card">
 		<button type="button">
-			<router-link :to="{ path: '/posts' }">Go to Posts Page</router-link>
+			<router-link
+				class="px-2 py-1 text-white bg-blue-500 rounded shadow hover:bg-blue-400"
+				:to="{ path: '/posts' }"
+				>Go to Posts Page</router-link
+			>
 		</button>
-		<p>
-			Edit
-			<code>components/HelloWorld.vue</code> to test HMR
-		</p>
 	</div>
-
-	<p>
-		Check out
-		<a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-			>create-vue</a
-		>, the official Vue + Vite starter
-	</p>
-	<p>
-		Install
-		<a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-		in your IDE for a better DX
-	</p>
-	<p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
-
-<style scoped>
-.read-the-docs {
-	color: #888;
-}
-</style>
